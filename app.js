1c7335fb6cb23218112a995c8d867d2abcc72844
unknown <mp922352612@gmail.com>
2019-08-30 21:53:48 +0800

    modified app.js

--- app.js
+++ app.js
@@ -12,11 +12,8 @@ const hbs = exphbs.create({
 })
 
 // setup handlebars engine and use .hbs as file extension
-
 app.engine(hbs.extname, hbs.engine, hbs.defaultLayout)
-// app.engine('.hbs', hbs.engine)
 app.set('view engine', hbs.extname)
-
 
 // setting body-parser
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -29,8 +26,6 @@ app.get('/', (req, res) => {
 app.post('/', (req, res) => {
   const target = req.body.target
   const sentence = generateTalk(target)
-
-
 
   res.render('index', {
     sentence,
